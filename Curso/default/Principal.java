f062c3fbd50477eb01efb07b60311487f00cf5eb
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-09-10 18:13:05 -0300

    bloque1.1 start threads

---
+++ Curso/default/Principal.java
@@ -0,0 +1,26 @@
+public class Principal extends Thread {
+    private int id;
+
+    public Principal(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        Principal h1= new Principal(1);
+        Principal h2= new Principal(2);
+
+        Principal h3= new Principal(3);
+
+        h1.start();
+        h2.start();
+        h3.start();
+
+        System.out.println("Hola");
+
+    }
+
+    public void run(){
+
+        System.out.println("Soy hilo "+this.id);
+    }
+}
--- tp3/tp3/Ejercicio6.java
+++ tp3/tp3/Ejercicio6.java
@@ -1,7 +1,7 @@
 public class Ejercicio6 {
     public static void main(String[] args) {
-        int matrizA[][] = new int[10][20];
-        int matrizB[][] = new int[20][10];
+        int matrizA[][] = new int[5][10];
+        int matrizB[][] = new int[10][5];
 
         cargarMatriz(matrizA);
         cargarMatriz(matrizB);
@@ -18,12 +18,37 @@ public class Ejercicio6 {
         }
     }
 
-    public static void mostrarMatriz(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[0].length; j++) {
-                System.out.print(matriz[i][j] + " ");
+    public static void intercambiarValores(int[][] matriz1, int[][] matriz2) {
+        for (int[] fila : matriz1) {
+            for (int elemento : fila) {
+
+                String valor = Integer.toString(elemento);
+
+                if (valor.length() < 2) {
+                    System.out.print("0" + elemento + " ");
+                } else {
+                    System.out.print(elemento + " ");
+                }
             }
             System.out.println();
         }
     }
-}
+
+    public static void mostrarMatriz(int[][] matriz) {
+        for (int[] fila : matriz) {
+            for (int elemento : fila) {
+
+                String valor = Integer.toString(elemento);
+
+                if (valor.length() < 2) {
+                    System.out.print("0" + elemento + " ");
+                } else {
+                    System.out.print(elemento + " ");
+                }
+            }
+            System.out.println();
+        }
+    }}
+
+// public Class Hilo{
+// }
